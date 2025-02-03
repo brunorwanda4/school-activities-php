10a884721d85896606d95db98b3b2a7eb1e25135
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-03 19:10:50 +0200

    start to design

--- school-magement/backend/config.php
+++ school-magement/backend/config.php
@@ -4,7 +4,7 @@
 $servername = "localhost"; // Database server
 $username = "root";        // Database username (default for local development)
 $password = "";            // Database password (leave empty for local or provide actual password)
-$dbname = "student_marks_db"; // Your database name (should match the one you've created)
+$dbname = "karine_db"; // Your database name (should match the one you've created)
 
 // Create connection
 $happy_conn = mysqli_connect($servername, $username, $password, $dbname);
--- school-magement/backend/login.php
+++ school-magement/backend/login.php
@@ -67,7 +67,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Login</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="flex items-center justify-center min-h-screen bg-gray-100">
+<body class="flex  min-h-screen bg-gray-100 w-full">
+    <div class="mb-6">
+        <img src="../images/13.jpg" alt="School Logo" class="mx-auto h-screen w-1/2 object-cover">
+    </div>
     <div class="bg-white shadow-lg rounded-lg p-8 w-full max-w-md border-t-4 border-red-500">
         <h2 class="text-2xl font-bold text-center text-red-600 mb-6">Login</h2>
         
