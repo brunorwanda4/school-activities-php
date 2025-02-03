b6d3a502428cfafa39d9711a1d24798f9e6f953f
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-03 08:51:05 +0200

    c

--- NIT-admin-client/backend/db_connection.php
+++ NIT-admin-client/backend/db_connection.php
@@ -1,6 +1,6 @@
 <!-- backend/db_connection.php -->
 <?php
-$karine_conn = mysqli_connect("localhost", "root", "", "student_marks_db");
+$karine_conn = mysqli_connect("localhost", "root", "", "happy_conn");
 
 if (!$karine_conn) {
     die("Connection failed: " . mysqli_connect_error());
